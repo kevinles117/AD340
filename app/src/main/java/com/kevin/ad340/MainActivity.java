0d4f4942e1bab7a56419cfee495d094853bd11e2
KevinLe <kevinles117@yahoo.com>
2019-04-16 22:26:36 -0700

    HW 2 completed

--- app/src/main/java/com/kevin/ad340/MainActivity.java
+++ app/src/main/java/com/kevin/ad340/MainActivity.java
@@ -1,13 +1,67 @@
 package com.kevin.ad340;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Button myButton;
+
+    String toast1 = "Toast 1 Worked!";
+    String toast2 = "Toast 2 Worked!";
+    String toast3 = "Toast 3 Worked!";
+    String toast4 = "Toast 4 Worked!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = (Button)findViewById(R.id.button1);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), toast1, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        myButton = (Button)findViewById(R.id.button2);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), toast2, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        myButton = (Button)findViewById(R.id.button3);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), toast3, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        myButton = (Button)findViewById(R.id.button4);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), toast4, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void buttonPressed(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "It worked!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
