0b3ae8e1522efca7b9a22f41746924fbeb1a4dad
KevinLe <kevinles117@yahoo.com>
2019-04-24 17:05:01 -0700

    Created a movierow.xml and a custom adapter, but it crashes when I click the button to go to the activity

--- app/src/main/java/com/kevin/ad340/MainActivity.java
+++ app/src/main/java/com/kevin/ad340/MainActivity.java
@@ -8,6 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -60,5 +68,39 @@ public class MainActivity extends AppCompatActivity {
     public void displayMovies(View v) {
         Intent intent = new Intent(this, DisplayZombieMovies.class);
         startActivity(intent);
+    }
+
+}
+
+
+class MySimpleArrayAdapter extends ArrayAdapter<String[]> {
+    private final Context context;
+    private final String[][] values;
+
+    public MySimpleArrayAdapter(Context context, String[][] values) {
+        super(context, -1, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.movierow, parent, false);
+        TextView titleView = (TextView) rowView.findViewById(R.id.movieTitle);
+        TextView yearView = (TextView) rowView.findViewById(R.id.yearTitle);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.imageMovie);
+        titleView.setText(values[position][0]);
+        yearView.setText(values[position][1]);
+        // change the icon for Windows and iPhone
+        //String s = values[position];
+        //if (s.startsWith("iPhone")) {
+        //    imageView.setImageResource(R.drawable.no);
+        //} else {
+        //    imageView.setImageResource(R.drawable.ok);
+        //}
+
+        return rowView;
     }
 }
